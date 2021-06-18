08b57bb787c9cfea1e28d434746a468c48dc8191
Bhannasa <77045671+Bhannasa@users.noreply.github.com>
2021-06-18 08:58:12 +0530

    final changes (font increase decrease applied)

--- script.js
+++ script.js
@@ -7,32 +7,42 @@ let curImg = 0;
 for (i = 1; i <= 5; i++) {
     img = document.getElementById("img" + i);
     img.src = images[i - 1];
-    // img.style.width = "100%";
-    // img.style.height = "60vh";
 }
 
-// let img_sec = document.getElementsByClassName("image-section")[0];
-// // slidingId = setInterval(slidePhotos, 2000);
-
-// function slidePhotos() {
-//     curImg++;
-//     curImg %= images.length;
-//     // console.log(images.length);
-//     console.log(img_sec, curImg);
-//     img_sec.style.left = "-" + (80 * curImg) + "vw";
-//     // img_sec.style.left = "-160vw";
-//     console.log(img_sec.style.left + "<");
-
-// }
-
 function changeFontSize(sign) {
-    // font = "larger";
-    if (sign == "+") document.querySelector("*").style.fontSize = "large";
-    else if (sign == "-") document.querySelector("*").style.fontSize = "small";
-    else document.querySelector("*").style.fontSize = "medium";
-    // font = document.querySelector("*").style.fontSize;
-    // console.log(font);
-
+    if (sign == "+"){
+        document.querySelector("*").style.fontSize = "large";
+        document.querySelector(".college-name").style.fontSize="120px";
+        document.querySelector(".navbar").style.fontSize="46px";
+        document.querySelector(".heading").style.fontSize="55px";
+        document.querySelector(".placements-and-nirf").style.fontSize="48px";
+        document.getElementsByClassName("some-lines-part")[0].style.fontSize="40px";
+        document.getElementsByClassName("some-lines-heading")[0].style.fontSize="50px";
+        document.getElementsByClassName("some-lines-part")[1].style.fontSize="40px";
+        document.getElementsByClassName("some-lines-heading")[1].style.fontSize="50px";
+    } 
+    else if (sign == "-"){
+        document.querySelector("*").style.fontSize = "small";
+        document.querySelector(".college-name").style.fontSize="80px";
+        document.querySelector(".navbar").style.fontSize="26px";
+        document.querySelector(".heading").style.fontSize="35px";
+        document.querySelector(".placements-and-nirf").style.fontSize="36px";
+        document.getElementsByClassName("some-lines-part")[0].style.fontSize="20px";
+        document.getElementsByClassName("some-lines-heading")[0].style.fontSize="30px";
+        document.getElementsByClassName("some-lines-part")[1].style.fontSize="20px";
+        document.getElementsByClassName("some-lines-heading")[1].style.fontSize="30px";
+    } 
+    else{
+        document.querySelector("*").style.fontSize = "medium";
+        document.querySelector(".college-name").style.fontSize="100px";
+        document.querySelector(".navbar").style.fontSize="36px";
+        document.querySelector(".heading").style.fontSize="45px";
+        document.querySelector(".placements-and-nirf").style.fontSize="46px";
+        document.getElementsByClassName("some-lines-part")[0].style.fontSize="30px";
+        document.getElementsByClassName("some-lines-heading")[0].style.fontSize="40px";
+        document.getElementsByClassName("some-lines-part")[1].style.fontSize="30px";
+        document.getElementsByClassName("some-lines-heading")[1].style.fontSize="40px";
+    } 
 }
 var loggedin=false;
 function loggedIn(){
@@ -41,5 +51,5 @@ function loggedIn(){
     document.getElementById("login").innerHTML="Logged In";
     document.getElementById("login")["href='#"];
     loggedin=true;
-    // window.close();
+    window.close();
 }
